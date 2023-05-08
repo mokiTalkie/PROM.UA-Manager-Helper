e19693cdb7ecece91a5b72706b6496d413c1a4ea
Владимир Касьянов <moki.main@gmail.com>
2023-05-08 13:40:49 +0300

    Added methods to generate category description, also added some functions for buttons. Refactored code, to be more readable

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,70 @@
 const product = $('#gen-product');
 const category = $('#gen-category');
-const user_input = $('#user-input');
-const tag_out = $('#tag-output')
-const desc_out = $("#description-output")
+const userInput = $('#user-input');
+const tagOut = $('#tag-output');
+const descOut = $("#description-output");
+const copyTags = $('.copy-tags');
+const copyDesc = $('.copy-desc');
 
+$(document).ready(function () {
+  $(product).click(function (e) {
+    e.preventDefault();
+    $(this).addClass('disabled button-loading');
+    $.ajax({
+      type: 'POST',
+      url: 'product',
+      data: { 'input': userInput.val() },
+      dataType: 'text',
+      success: function (response) {
+        $(this).removeClass('button-loading disabled');
+        const JSONObj = JSON.parse(response);
+        tagOut.val(JSONObj.tags);
+        descOut.val(JSONObj.description);
+      }.bind(this)
+    });
+  });
 
-$(product).click(function(e) {
-  e.preventDefault();
-  
-  $.ajax({
-    type: 'POST',
-    url: 'product',
-    data: { 'input': $(user_input).val() },
-    dataType: 'text',
-    success: function(response) {
-      var JSONObj = JSON.parse(response)
-      $(tag_out).val(JSONObj.tags)
-      $(desc_out).val(JSONObj.description);
+  $(category).click(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: 'category',
+      data: { 'input': userInput.val() },
+      dataType: 'text',
+      success: function (response) {
+        const JSONObj = JSON.parse(response);
+        tagOut.val(JSONObj.tags);
+        descOut.val(JSONObj.description);
+      }
+    });
+  });
+
+  function copyText(btn, text) {
+    if (text !== '') {
+      const $temp = $("<textarea>");
+      $("body").append($temp);
+      $temp.val(text).select();
+      document.execCommand("copy");
+      $temp.remove();
+      $(btn).text("Copied")
+        .css("background-color", "#28a745")
+        .prop("disabled", true);
+        
+      setTimeout(function () {
+        $(btn).text("Copy")
+          .css("background-color", "#4e5d6c")
+          .prop("disabled", false);
+      }, 2000);
+    } else {
+      alert("Область із вихідним текстом порожня.");
     }
+  }
+
+  $(copyTags).click(function () {
+    copyText(this, tagOut.val());
+  });
+
+  $(copyDesc).click(() => {
+    copyText(copyDesc, descOut.val());
   });
 });
