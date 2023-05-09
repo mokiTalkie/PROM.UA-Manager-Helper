3d2a268dcfeff5203d4e39f8437a63d7ed5182f0
Владимир Касьянов <moki.main@gmail.com>
2023-05-09 18:29:13 +0300

    Prepairing for deploy

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask
 from routes.additional import additional
 from routes.tools import tools
 
-app = Flask(__name__, template_folder='templates')
-app.register_blueprint(blueprint=additional)
-app.register_blueprint(blueprint=tools)
+
+def create_app() -> Flask:
+    app = Flask(__name__, template_folder='templates')
+    app.register_blueprint(blueprint=additional)
+    app.register_blueprint(blueprint=tools)
+
+    return app
 
 
-if __name__ == '__main__':
-    app.run(
-        debug=True
-    )
+app = create_app()
