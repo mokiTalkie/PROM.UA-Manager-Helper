41ea0f9c22dcd5273ecce48438889288dbf20ced
Владимир Касьянов <moki.main@gmail.com>
2023-05-09 18:18:33 +0300

    Some changes to routes

--- routes/tools.py
+++ routes/tools.py
@@ -1,13 +1,29 @@
 from flask import Blueprint, jsonify, render_template, request
 
 from external_api.openai_api import TextHelper
+from templates.text_templates import (
+    PRODUCT_TOOL_HEAD,
+    PRODUCT_TOOL_DESCRIPTION,
+    CATEGORY_TOOL_HEAD,
+    CATEGORY_TOOL_DESCRIPTION,
+)
 
 tools = Blueprint("tool", __name__, url_prefix="/tool/")
 
 
 @tools.get("/product")
 def product_load() -> str:
-    return render_template("main_template.html", route_product='product', route_category='product', content=render_template("tool.html", source='product'))
+    return render_template(
+        "main_template.html",
+        route_product="product",
+        route_category="category",
+        content=render_template(
+            "tool.html",
+            source="product",
+            tool_header=PRODUCT_TOOL_HEAD,
+            tool_description=PRODUCT_TOOL_DESCRIPTION,
+        ),
+    )
 
 
 @tools.post("/product")
@@ -22,7 +38,17 @@ def generate_product() -> str:
 
 @tools.get("/category")
 def category_load() -> str:
-    return render_template("main_template.html", route_product='product', route_category='product', content=render_template("tool.html", source='category'))
+    return render_template(
+        "main_template.html",
+        route_product="product",
+        route_category="category",
+        content=render_template(
+            "tool.html",
+            source="category",
+            tool_header=CATEGORY_TOOL_HEAD,
+            tool_description=CATEGORY_TOOL_DESCRIPTION,
+        ),
+    )
 
 
 @tools.post("/category")
