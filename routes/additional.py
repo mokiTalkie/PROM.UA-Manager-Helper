b148a8dc066d37bec4c88150ec678c40d6682919
Владимир Касьянов <moki.main@gmail.com>
2023-05-29 12:32:45 +0300

    Rewrited routes under FastAPI

--- routes/additional.py
+++ routes/additional.py
@@ -1,22 +1,32 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+
+from routes import templates
 
 additional = APIRouter(prefix="", default_response_class=HTMLResponse)
-
-templates = Jinja2Templates(directory="templates")
 
 
 @additional.get("/")
 async def index(request: Request) -> str:
+    """
+    Render the index page.
+    """
     return templates.TemplateResponse(name="index.html", context={"request": request})
 
 
 @additional.get("/about")
 async def about(request: Request) -> str:
+    """
+    Render the about page.
+    """
     return templates.TemplateResponse(name="about.html", context={"request": request})
 
 
 @additional.get("/contacts")
 async def contacts(request: Request) -> str:
-    return templates.TemplateResponse(name="contacts.html", context={"request": request})
+    """
+    Render the contacts page.
+    """
+    return templates.TemplateResponse(
+        name="contacts.html", context={"request": request}
+    )
