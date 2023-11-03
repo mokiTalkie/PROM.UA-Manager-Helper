68c2b1a24b3b8b15989f6091fc95251f5343ba81
Владимир Касьянов <moki.main@gmail.com>
2023-11-03 12:48:17 +0200

    FreeGPT api methods added

---
+++ external_api/free_gpt_api.py
@@ -0,0 +1,186 @@
+import g4f
+from g4f.models import gpt_35_turbo
+
+from templates.text_templates import (
+    CATEGORY_DESCRIPTION_PROMPT,
+    CATEGORY_SEARCH_QUERIES_PROMPT,
+    PRODUCT_DESCRIPTION_PROMPT,
+    PRODUCT_SEARCH_QUERY_PROMPT,
+)
+
+
+class FreeGPTTextHelper:
+    """
+    A class that provides helper functions for generating product search queries and descriptions.
+    """
+
+    @staticmethod
+    async def get_product_search_queries(product_name: str) -> str:
+        """
+        Generates search queries for a given product using OpenAI's text generation API.
+
+        Parameters:
+        -----------
+        product : str
+            The name of the product for which to generate search queries.
+
+        Returns:
+        --------
+        str
+            A string containing the generated search queries."""
+
+        # ai_response = await openai.Completion.acreate(
+        #     engine=os.getenv("AI_TEXT_MODEL"),
+        #     prompt=PRODUCT_SEARCH_QUERY_PROMPT.format(product_name),
+        #     temperature=0.5,
+        #     max_tokens=2000,
+        #     n=1,
+        #     stop=None,
+        #     timeout=None,
+        #     presence_penalty=0.0,
+        #     frequency_penalty=0.0,
+        #     best_of=1,
+        #     # user=None,
+        #     logprobs=None,
+        #     echo=False
+        # )
+
+        # return ai_response["choices"][0]["text"]
+        ai_response = await g4f.ChatCompletion.create_async(
+            model=gpt_35_turbo,
+            messages=[
+                {
+                    "role": "user",
+                    "content": PRODUCT_SEARCH_QUERY_PROMPT.format(product_name),
+                }
+            ],
+        )
+
+        return ai_response
+
+    @staticmethod
+    async def get_product_description(product_name: str) -> str:
+        """
+        Generates a promotional description for a given product using OpenAI's text generation API.
+
+        Parameters:
+        -----------
+        product : str
+            The name of the product for which to generate a promotional description.
+
+        Returns:
+        --------
+        str
+            A string containing the generated promotional description.
+        """
+
+        # ai_response = await openai.Completion.acreate(
+        #     engine=os.getenv("AI_TEXT_MODEL"),
+        #     prompt=PRODUCT_DESCRIPTION_PROMPT.format(product_name),
+        #     temperature=0.5,
+        #     max_tokens=2000,
+        #     n=1,
+        #     stop=None,
+        #     timeout=None,
+        #     presence_penalty=0.0,
+        #     frequency_penalty=0.0,
+        #     best_of=1,
+        #     # user=None,
+        #     logprobs=None,
+        #     echo=False,
+        # )
+
+        # return ai_response["choices"][0]["text"]
+        ai_response = await g4f.ChatCompletion.create_async(
+            model=gpt_35_turbo,
+            messages=[
+                {
+                    "role": "user",
+                    "content": PRODUCT_DESCRIPTION_PROMPT.format(product_name),
+                }
+            ],
+        )
+
+        return ai_response
+
+    @staticmethod
+    async def get_category_search_queries(category_name: str) -> str:
+        """
+        Generate search tags for the given product category.
+        These tags should be adapted for the e-commerce site.
+        Return only the search tags themselves, separated by a comma and on a single line.
+
+        Args:
+        category_name (str): The name of the product category.
+
+        Returns:
+        str: A string containing the generated search tags."""
+
+        # ai_response = await openai.Completion.acreate(
+        #     engine=os.getenv("AI_TEXT_MODEL"),
+        #     prompt=CATEGORY_SEARCH_QUERIES_PROMPT.format(category_name),
+        #     max_tokens=2000,
+        #     temperature=0.5,
+        #     n=1,
+        #     stop=None,
+        #     timeout=None,
+        #     presence_penalty=0.0,
+        #     frequency_penalty=0.0,
+        #     best_of=1,
+        #     # user=None,
+        #     logprobs=None,
+        #     echo=False,
+        # )
+
+        # return ai_response["choices"][0]["text"]
+        ai_response = await g4f.ChatCompletion.create_async(
+            model=gpt_35_turbo,
+            messages=[
+                {
+                    "role": "user",
+                    "content": CATEGORY_SEARCH_QUERIES_PROMPT.format(category_name),
+                }
+            ],
+        )
+
+        return ai_response
+
+    @staticmethod
+    async def get_category_description(category_name: str) -> str:
+        """
+        Generate a description for the given product group. The description should not be redundant but should sell. Try to keep it to 2-3 paragraphs and about 100 words. Return only the description itself, without your own comments.
+
+        Args:
+        category_name (str): The name of the product group.
+
+        Returns:
+        str: A string containing the generated product group description."""
+
+        # ai_response = await openai.Completion.acreate(
+        #     engine=os.getenv("AI_TEXT_MODEL"),
+        #     prompt=CATEGORY_DESCRIPTION_PROMPT.format(category_name),
+        #     max_tokens=2000,
+        #     temperature=0.5,
+        #     n=1,
+        #     stop=None,
+        #     timeout=None,
+        #     presence_penalty=0.0,
+        #     frequency_penalty=0.0,
+        #     best_of=1,
+        #     # user=None,
+        #     logprobs=None,
+        #     echo=False,
+        # )
+
+        # return ai_response["choices"][0]["text"]
+        ai_response = await g4f.ChatCompletion.create_async(
+            model=gpt_35_turbo,
+            messages=[
+                {
+                    "role": "user",
+                    "content": CATEGORY_DESCRIPTION_PROMPT.format(category_name),
+                }
+            ],
+        )
+
+        return ai_response
