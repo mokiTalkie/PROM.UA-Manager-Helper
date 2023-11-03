f6a92907aea8edb17a112e473ac8cff02a56e37b
Владимир Касьянов <moki.main@gmail.com>
2023-11-03 14:33:38 +0200

    Repeat buttons methods added

--- static/js/script.js
+++ static/js/script.js
@@ -7,9 +7,74 @@ const descOut = $("#description-output");
 const copyTags = $('.copy-tags');
 const copyDesc = $('.copy-desc');
 const preloader = $('.preloader');
+const repeatProductTag = $('#repeat-product-queries')
+const repeatProductDesc = $('#repeat-product-description')
+const repeatCategoryTag = $('#repeat-category-queries')
+const repeatCategoryDesc = $('#repeat-category-description')
 
 // When the document is ready
 $(document).ready(function () {
+
+  $(repeatProductTag).click(function (e) {
+    e.preventDefault();
+    $(preloader).show();
+
+    $.ajax({
+      type: 'POST',
+      url: 'repeat',
+      contentType: 'application/json',
+      data: JSON.stringify({ 'input': userInput.val(), 'target': 'product_tags' }),
+      success: function (response) {
+        $(preloader).hide();
+        tagOut.val(response.text)
+      }
+    });
+  });
+  $(repeatProductDesc).click(function (e) {
+    e.preventDefault();
+    $(preloader).show();
+
+    $.ajax({
+      type: 'POST',
+      url: 'repeat',
+      contentType: 'application/json',
+      data: JSON.stringify({ 'input': userInput.val(), 'target': 'product_description' }),
+      success: function (response) {
+        $(preloader).hide();
+        descOut.val(response.text)
+      }
+    });
+  });
+  $(repeatCategoryTag).click(function (e) {
+    e.preventDefault();
+    $(preloader).show();
+
+    $.ajax({
+      type: 'POST',
+      url: 'repeat',
+      contentType: 'application/json',
+      data: JSON.stringify({ 'input': userInput.val(), 'target': 'category_tags' }),
+      success: function (response) {
+        $(preloader).hide();
+        tagOut.val(response.text)
+      }
+    });
+  });
+  $(repeatCategoryDesc).click(function (e) {
+    e.preventDefault();
+    $(preloader).show();
+
+    $.ajax({
+      type: 'POST',
+      url: 'repeat',
+      contentType: 'application/json',
+      data: JSON.stringify({ 'input': userInput.val(), 'target': 'category_description' }),
+      success: function (response) {
+        $(preloader).hide();
+        descOut.val(response.text)
+      }
+    });
+  });
   // When the 'product' button is clicked
   $(product).click(function (e) {
     e.preventDefault();
@@ -65,7 +130,7 @@ $(document).ready(function () {
       $(btn).text("Скопійовано")
         .css("background-color", "#5a5a5a")
         .prop("disabled", true);
-        
+
       // Reset the button text and styling after 2 seconds
       setTimeout(function () {
         $(btn).text("Копіювати")
